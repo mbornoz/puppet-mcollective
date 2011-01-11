b3d345c7cb49d693f975c4cb5c0f2723a50de7e1
Atha Kouroussis <athak@vurbia.com>
2011-01-11 09:55:42 -0300

    Fixed wrong file source for meta registration plugin

--- manifests/classes/plugins/registration/meta.pp
+++ manifests/classes/plugins/registration/meta.pp
@@ -7,6 +7,7 @@ class mcollective::plugins::registration::meta inherits mcollective::plugins::ba
 	mcollective::plugin { "meta":
 		ensure       => present,
 		type         => "registration",
+		source       => "puppet:///modules/mcollective/plugins/registration/meta.rb",
 		config       => true,
 		config_order => 15
 	}
