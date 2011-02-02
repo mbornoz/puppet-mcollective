16b49031f7c95df93ee56dd3591c2aab647083d2
Atha Kouroussis <akouroussis@gmail.com>
2011-02-02 20:40:27 -0300

    mcollective::plugins::connector::stomp doesn't need to inherit mcollective::plugins::base

--- manifests/classes/plugins/connector/stomp.pp
+++ manifests/classes/plugins/connector/stomp.pp
@@ -1,6 +1,6 @@
 # Class: mcollective::plugins::connector::stomp inherits mcollective::plugins::base
 #
 #
-class mcollective::plugins::connector::stomp inherits mcollective::plugins::base {
+class mcollective::plugins::connector::stomp {
 	class { "ruby::stomp": version => $mcollective::params::stomp_version }
 }
