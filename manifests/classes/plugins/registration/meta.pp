5d2a84c6ac8ae3ee1863cb82f604625df5c5ce4a
Atha Kouroussis <akouroussis@gmail.com>
2011-02-02 21:59:35 -0300

    Fix the source of the meta registation plugin

--- manifests/classes/plugins/registration/meta.pp
+++ manifests/classes/plugins/registration/meta.pp
@@ -5,6 +5,7 @@ class mcollective::plugins::registration::meta {
 	mcollective::plugin { "meta":
 		ensure       => present,
 		type         => "registration",
+		source       => "puppet:///modules/mcollective/plugins/puppetlabs/registration/meta.rb",
 		config       => true,
 		config_order => 15
 	}
