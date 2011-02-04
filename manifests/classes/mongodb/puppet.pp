c2a6c75f072089f929d4dc772a3546036266b762
Atha Kouroussis <akouroussis@gmail.com>
2011-02-04 19:45:52 -0300

    Fix paths for mongodb query files

--- manifests/classes/mongodb/puppet.pp
+++ manifests/classes/mongodb/puppet.pp
@@ -1,7 +1,7 @@
 # Class: mcollective::puppet
 #
 #
-class mcollective::mongodb::puppet inherits mcollective::mongodb {
+class mcollective::mongodb::puppet {
 	File {
 		owner => root,
 		group => root,
@@ -10,21 +10,21 @@ class mcollective::mongodb::puppet inherits mcollective::mongodb {
 	
 	file { "${mcollective::params::ruby_puppet_dir}/util/mongoquery.rb":
 		ensure => present,
-		source => "puppet:///modules/mcollective/plugins/agent/registration-mongodb/puppet/mongoquery.rb"
+		source => "puppet:///modules/mcollective/plugins/puppetlabs/agent/registration-mongodb/puppet/mongoquery.rb"
 	}
 	
 	file { "${mcollective::params::ruby_puppet_dir}/parser/functions/load_node.rb":
 		ensure => present,
-		source => "puppet:///modules/mcollective/plugins/agent/registration-mongodb/puppet/load_node.rb"
+		source => "puppet:///modules/mcollective/plugins/puppetlabs/agent/registration-mongodb/puppet/load_node.rb"
 	}
 	
 	file { "${mcollective::params::ruby_puppet_dir}/parser/functions/search_nodes.rb":
 		ensure => present,
-		source => "puppet:///modules/mcollective/plugins/agent/registration-mongodb/puppet/search_nodes.rb"
+		source => "puppet:///modules/mcollective/plugins/puppetlabs/agent/registration-mongodb/puppet/search_nodes.rb"
 	}
 	
 	file { "${mcollective::params::ruby_puppet_dir}/parser/functions/search_setup.rb":
 		ensure => present,
-		source => "puppet:///modules/mcollective/plugins/agent/registration-mongodb/puppet/search_setup.rb"
+		source => "puppet:///modules/mcollective/plugins/puppetlabs/agent/registration-mongodb/puppet/search_setup.rb"
 	}
 }
